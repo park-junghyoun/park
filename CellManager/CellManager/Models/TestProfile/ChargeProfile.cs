bacaf3a8eef593b144087115281c38706cd410f2
park-junghyoun <junghyoun.p@gmail.com>
2025-09-04 16:44:37 +0900

    Add charge mode selection and time/capacity inputs

--- CellManager/CellManager/Models/TestProfile/ChargeProfile.cs
+++ CellManager/CellManager/Models/TestProfile/ChargeProfile.cs
@@ -37,6 +37,39 @@ namespace CellManager.Models.TestProfile
         [NotifyPropertyChangedFor(nameof(PreviewText))]
         private TimeSpan _chargeTime;
 
+        [ObservableProperty]
+        private int _chargeHours;
+
+        [ObservableProperty]
+        private int _chargeMinutes;
+
+        [ObservableProperty]
+        private int _chargeSeconds;
+
+        private bool _updatingChargeTime;
+
+        partial void OnChargeHoursChanged(int value) => UpdateChargeTime();
+        partial void OnChargeMinutesChanged(int value) => UpdateChargeTime();
+        partial void OnChargeSecondsChanged(int value) => UpdateChargeTime();
+
+        partial void OnChargeTimeChanged(TimeSpan value)
+        {
+            if (_updatingChargeTime) return;
+            _updatingChargeTime = true;
+            ChargeHours = value.Hours;
+            ChargeMinutes = value.Minutes;
+            ChargeSeconds = value.Seconds;
+            _updatingChargeTime = false;
+        }
+
+        private void UpdateChargeTime()
+        {
+            if (_updatingChargeTime) return;
+            _updatingChargeTime = true;
+            ChargeTime = new TimeSpan(ChargeHours, ChargeMinutes, ChargeSeconds);
+            _updatingChargeTime = false;
+        }
+
         public string PreviewText => ChargeMode switch
         {
             ChargeMode.ChargeByCapacity => $"Current: {ChargeCurrent} A, Cutoff: {ChargeCutoffVoltage} V, Capacity: {ChargeCapacityMah} mAh",
