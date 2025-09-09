a4b5b4a075aab9f55bb811d1ecbc2de703a62cf7
park-junghyoun <junghyoun.p@gmail.com>
2025-09-09 10:13:21 +0900

    Add reusable TimeSpanEditor control

---
+++ CellManager/CellManager/Views/Controls/TimeSpanEditor.xaml.cs
@@ -0,0 +1,52 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CellManager.Views.Controls
+{
+    public partial class TimeSpanEditor : UserControl
+    {
+        public TimeSpanEditor()
+        {
+            InitializeComponent();
+        }
+
+        public int Hours
+        {
+            get => (int)GetValue(HoursProperty);
+            set => SetValue(HoursProperty, value);
+        }
+
+        public static readonly DependencyProperty HoursProperty =
+            DependencyProperty.Register(
+                nameof(Hours),
+                typeof(int),
+                typeof(TimeSpanEditor),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public int Minutes
+        {
+            get => (int)GetValue(MinutesProperty);
+            set => SetValue(MinutesProperty, value);
+        }
+
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register(
+                nameof(Minutes),
+                typeof(int),
+                typeof(TimeSpanEditor),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public int Seconds
+        {
+            get => (int)GetValue(SecondsProperty);
+            set => SetValue(SecondsProperty, value);
+        }
+
+        public static readonly DependencyProperty SecondsProperty =
+            DependencyProperty.Register(
+                nameof(Seconds),
+                typeof(int),
+                typeof(TimeSpanEditor),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    }
+}
