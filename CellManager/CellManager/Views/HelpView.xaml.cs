6beff5f4f4d8fc615e3901e976babfae62392341
park-junghyoun <junghyoun.p@gmail.com>
2025-09-11 10:13:47 +0900

    Add Help view for Help tab

---
+++ CellManager/CellManager/Views/HelpView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CellManager.Views
+{
+    public partial class HelpView : UserControl
+    {
+        public HelpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
